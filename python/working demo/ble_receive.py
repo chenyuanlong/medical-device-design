3054502a05b86b05442c007fee7fe060a8628f53
chenyuanlong <chenyuanlong@users.noreply.github.com>
2017-11-01 00:19:41 +0800

    change transmission method to bleuart

--- python/working demo/ble_receive.py
+++ python/working demo/ble_receive.py
@@ -24,20 +24,44 @@ manager = pylab.get_current_fig_manager()
 
 values = [0 for x in range(50)]
 
-cmd = 'gatttool -b D3:E0:D2:08:2B:8F -t random --char-write-req -a 0x2a -n 0100 --listen'
+#cmd = 'gatttool -b D3:E0:D2:08:2B:8F -t random --char-write-req -a 0x2a -n 0100 --listen'
+cmd = 'gatttool -b D3:E0:D2:08:2B:8F -t random --char-write-req -a 0x23 -n 0100 --listen'
 p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-
+temp = []
 def getData(arg):
-    global p
+    global p,temp
     line = p.stdout.readline().decode("utf-8")
     if('Characteristic' in line):
         output = ''
     else:
-        line = p.stdout.readline().decode("utf-8").split(' ')
-        output = line[5]+line[6]
-        output =  int("0x"+output,16)
-        values.append(output)
-        print("The amplitude is: "+str(output))
+        #line = combine with previous list after the last space
+        line = line.split(' ')[5:-1]
+        line = temp + line
+        temp = []
+        if not('0a' in line) or line[-1]=='0a':
+         line = ''.join(chr(int(i,16)) for i in line)
+         output = line.split(' ')
+         for i in output:
+          if i!='':
+           #values.append(int(i))
+           output = [x for x in i.split('\n') if x!='']
+           values.extend(output)
+           #print(output)
+        else:
+         #last space character in list
+         last_space = max(loc for loc, val in enumerate(line) if val == '0a')
+         last_space += 1
+         temp = line[last_space:]#put characters after last space in next list
+         line = line[:last_space]#truncate characters before last space
+         line = ''.join(chr(int(i,16)) for i in line)
+         output = line.split(' ')
+         for i in output:
+          if i!='':
+           #values.append(int(i))
+           output = [x for x in i.split('\n') if x!='']
+           values.extend(output)
+           #print(output)
+
 
 def RealtimePloter(arg):
   global values
@@ -47,9 +71,9 @@ def RealtimePloter(arg):
   manager.canvas.draw()
   #manager.show()
 
-timer = fig.canvas.new_timer(interval=20)
+timer = fig.canvas.new_timer(interval=100)
 timer.add_callback(RealtimePloter, ())
-timer2 = fig.canvas.new_timer(interval=20)
+timer2 = fig.canvas.new_timer(interval=100)
 timer2.add_callback(getData, ())
 timer.start()
 timer2.start()
